e648bfa1f43b6a88c4133eb0da82cf008ff3da16
Franz Pfeiffer <Franz.Pfeiffer@ucr.ac.cr>
2021-05-24 18:28:38 -0600

    codigo con comentarios adicionados

--- VacunasSemanales.java
+++ VacunasSemanales.java
@@ -1,5 +1,17 @@
 public class VacunasSemanales {
-    
+/**
+ * Clase con los metodos para convertir los arreglos en lo que nos pedia el ejercicio
+ * Se encarga de iniciar o correr el juego.
+ * @author Franz Pfeiffer
+ * @version 5/24/2021
+ * 
+ */
+
+
+    /**
+    * convierte el arreglo en un arreglo dividido por semanas en el metodo main hay un codigo como comentario para imprimirlo
+    * 
+    */
     public String [][] arregloSemanas (String [][] matriz){
         String [][] segundaMatriz = new String[8][8];
         //Itera sobre las filas
@@ -12,6 +24,10 @@ public class VacunasSemanales {
     return segundaMatriz;
     }
 
+   /**
+    * convierte el arreglo en un arreglo con la cantidad total semanal
+    * 
+    */
     public String [][] cantidadTotalSemanal (String [][] matriz){
         String [][] segundaMatriz = new String[2][8]; //segunda matriz para retornar el Total de cada semana
         int contador; //Para sumar las vacunas de cada region de esa semana
@@ -29,13 +45,20 @@ public class VacunasSemanales {
         return segundaMatriz;
     }
 
+     /**
+    * utiliza la seleccion por teclado del main para imprimir el arreglo correspondiente a esa semana
+    * 
+    */
     public void imprimirTotalSemana(int seleccion, String [][] matriz){
         if(seleccion >= 12 || seleccion <= 19)
          System.out.println(matriz[1][seleccion-12]);
         else
             System.out.println("Seleccion invalida");
     }
-
+ /**
+    * convierte el arreglo en un arreglo con la diferencia entre cada semana
+    * 
+    */
     public String [][] diferenciaEntreSemanas (String [][] matriz){
         String [][] segundaMatriz = new String[2][8]; //segunda matriz para retornar el Total de cada semana
         int contador; //Para sumar las vacunas de cada region de esa semana
